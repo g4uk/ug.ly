5828aa12ab80ca3a91734c469cecc4ca403454eb
Roman Gnatyuk <gnat.dev@gmail.com>
2017-03-15 11:00:42 +0200

    .

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,8 +4,15 @@ class StatsController < ApplicationController
 
     @link = Link.find_by(ugly_path: permitted_params[:shorten_path])
     @stats = @link.stats.duration_from(10.days)
-    unless @link
-      render template: 'errors/link_not_found', layout: 'layouts/application', status: 404
-    end
+
+    @total_clicks = @stats.to_a.count
+
+    @time_chart_data = @stats.group_by_day(:created_at).count
+    @referrers_chart_data = @stats.group(:source).count
+    @browsers_chart_data = @stats.group(:browser_name).count
+    @geo_chart_data = @stats.group(:country).count
+    @os_chart_data = @stats.group(:os).count
+
+    render template: 'errors/link_not_found', layout: 'layouts/application', status: 404 unless @link
   end
 end
